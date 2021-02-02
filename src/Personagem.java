eff9513bbec3481ba62e584a88ad232fb0eaca9c
João Pedro de Moura Góes <jp.mouragoes@gmail.com>
2021-02-01 23:28:33 -0300

    Update Personagem.java

--- src/Personagem.java
+++ src/Personagem.java
@@ -155,7 +155,7 @@ public abstract class Personagem {
 
     void ativarItem(String s){
         for (Pocoes p:getInventario()){
-            if(s.equals(p.getName()){
+            if(s.equals(p.getName())){
                 p.active(this);
                 int index = this.inventario.indexOf(p);
                 this.inventario = removeItem(this.inventario, index);
